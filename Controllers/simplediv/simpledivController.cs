79a2025bfee06dd80b848f6bef424c30e1e1c0d1
qingxue0606 <77055664@qq.com>
2020-05-27 16:03:18 +0800

    11
    Signed-off-by: qingxue0606 <77055664@qq.com>

---
+++ Controllers/simplediv/simpledivController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+namespace selectdemo.Controllers.simplediv
+{
+    public class simpledivController : Controller
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public simpledivController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        public IActionResult Word()
+        {
+            PageOfficeNetCore.PageOfficeCtrl pageofficeCtrl = new PageOfficeNetCore.PageOfficeCtrl(Request);
+            pageofficeCtrl.ServerPage = "../PageOffice/POServer";
+            //添加自定义按钮
+            pageofficeCtrl.AddCustomToolButton("保存", "SaveFile()", 1);
+            pageofficeCtrl.AddCustomToolButton("打印", "PrintFile()", 6);
+            pageofficeCtrl.AddCustomToolButton("关闭", "CloseFile()", 21);
+            //设置保存页面
+            pageofficeCtrl.SaveFilePage = "SaveDoc";
+            //打开Word文档
+            pageofficeCtrl.WebOpen("/simplediv/doc/test.doc", PageOfficeNetCore.OpenModeType.docRevisionOnly, "tom");
+            ViewBag.POCtrl = pageofficeCtrl.GetHtmlCode("PageOfficeCtrl1");
+            return View();
+        }
+
+        public async Task<ActionResult> SaveDoc()
+        {
+            PageOfficeNetCore.FileSaver fs = new PageOfficeNetCore.FileSaver(Request, Response);
+            await fs.LoadAsync();
+            string webRootPath = _webHostEnvironment.WebRootPath;
+            fs.SaveToFile(webRootPath + "/simplediv/doc/" + fs.FileName);
+            fs.Close();
+            return Content("OK");
+        }
+    }
+}
---
+++ Controllers/simpleselect/simpleselectController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+namespace selectdemo.Controllers.simpleselect
+{
+    public class simpleselectController : Controller
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public simpleselectController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+        public IActionResult Word()
+        {
+            PageOfficeNetCore.PageOfficeCtrl pageofficeCtrl = new PageOfficeNetCore.PageOfficeCtrl(Request);
+            pageofficeCtrl.ServerPage = "../PageOffice/POServer";
+            pageofficeCtrl.Titlebar = false;
+            pageofficeCtrl.OfficeToolbars = false;
+            pageofficeCtrl.Menubar = false;
+            pageofficeCtrl.CustomToolbar = false;
+
+            //设置保存页面
+            pageofficeCtrl.SaveFilePage = "SaveDoc";
+            //打开Word文档
+            pageofficeCtrl.WebOpen("/simpleselect/doc/test.doc", PageOfficeNetCore.OpenModeType.docNormalEdit, "tom");
+            ViewBag.POCtrl = pageofficeCtrl.GetHtmlCode("PageOfficeCtrl1");
+            return View();
+        }
+        public async Task<ActionResult> SaveDoc()
+        {
+            PageOfficeNetCore.FileSaver fs = new PageOfficeNetCore.FileSaver(Request, Response);
+            await fs.LoadAsync();
+            string webRootPath = _webHostEnvironment.WebRootPath;
+            fs.SaveToFile(webRootPath + "/simpleselect/doc/" + fs.FileName);
+            fs.Close();
+            return Content("OK");
+        }
+    }
+}
